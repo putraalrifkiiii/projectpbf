20eb3a48aeb01fd9ae80cca8dcc22c5512d1e7c4
putraalrifkiiii <putraalrifkiiii19@gmail.com>
2026-04-14 15:38:05 +0700

    Add project files

---
+++ app/Controllers/Layout.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Layout extends BaseController
+{
+    public function index(): string
+    {
+        return view('layout');
+    }
+}
---
+++ app/Views/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+</head>
+<body>
+<button class="btn btn-primary">Bootstrap Aktif</button>
+<script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+</body>
+</html>

922ff6091f8b8f036bede14111104d64d3806ee8
Wazzelodon <fawazirwanramadhan01@gmailcom>
2026-06-02 10:44:19 +0700

    fixed

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,21 +5,34 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/login', 'Auth::login');
-// $routes->post('/auth/processLogin', 'Auth::processLogin');
-// $routes->get('/register', 'Auth::register');
-// $routes->post('/auth/processRegister', 'Auth::processRegister');
-// $routes->get('/logout', 'Auth::logout');
 
-// 1. Rute Dashboard HARUS BERDIRI SENDIRI DI SINI
+// Route utama
+$routes->get('/', function () {
+    return redirect()->to('/dashboard');
+});
+
+// Auth
+$routes->get('/login', 'Auth::login');
+$routes->post('/auth/processLogin', 'Auth::processLogin');
+
+$routes->get('/register', 'Auth::register');
+$routes->post('/auth/processRegister', 'Auth::processRegister');
+
+$routes->get('/logout', 'Auth::logout');
+
+// Dashboard
 $routes->get('/dashboard', 'Dashboard::index', ['filter' => 'login']);
 
-// 2. Rute Produk TETAP DI DALAM GRUP
+// Produk
 $routes->group('produk', ['filter' => 'login'], function ($routes) {
+
     $routes->get('/', 'Produk::index');
+
     $routes->get('create', 'Produk::create');
     $routes->post('store', 'Produk::store');
+
     $routes->get('edit/(:num)', 'Produk::edit/$1');
     $routes->post('update/(:num)', 'Produk::update/$1');
+
     $routes->delete('delete/(:num)', 'Produk::delete/$1');
 });
